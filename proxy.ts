5c21b9c9b637bde7c3a7a4a0b7d17ee3b0558105
hellvita <seredaolga03@gmail.com>
2026-01-03 20:59:29 +0200

    fix the proxy

--- proxy.ts
+++ proxy.ts
@@ -21,50 +21,46 @@ export async function proxy(request: NextRequest) {
     pathname.startsWith(route)
   );
 
-  if (isPrivateRoute) {
-    if (!accessToken) {
-      if (refreshToken) {
-        const data = await checkSession();
-        const setCookie = data.headers["set-cookie"];
+  if (!accessToken) {
+    if (refreshToken) {
+      const data = await checkSession();
+      const setCookie = data.headers["set-cookie"];
 
-        if (setCookie) {
-          const cookieArray = Array.isArray(setCookie)
-            ? setCookie
-            : [setCookie];
+      if (setCookie) {
+        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
 
-          for (const cookieStr of cookieArray) {
-            const parsed = parse(cookieStr);
+        for (const cookieStr of cookieArray) {
+          const parsed = parse(cookieStr);
 
-            const options = {
-              expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-              path: parsed.Path,
-              maxAge: Number(parsed["Max-Age"]),
-            };
+          const options = {
+            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+            path: parsed.Path,
+            maxAge: Number(parsed["Max-Age"]),
+          };
 
-            if (parsed.accessToken)
-              cookieStore.set("accessToken", parsed.accessToken, options);
+          if (parsed.accessToken)
+            cookieStore.set("accessToken", parsed.accessToken, options);
 
-            if (parsed.refreshToken)
-              cookieStore.set("refreshToken", parsed.refreshToken, options);
-          }
-
-          if (isPublicRoute)
-            return NextResponse.redirect(new URL("/", request.url), {
-              headers: { Cookie: cookieStore.toString() },
-            });
-
-          if (isPrivateRoute)
-            return NextResponse.next({
-              headers: { Cookie: cookieStore.toString() },
-            });
+          if (parsed.refreshToken)
+            cookieStore.set("refreshToken", parsed.refreshToken, options);
         }
+
+        if (isPublicRoute)
+          return NextResponse.redirect(new URL("/", request.url), {
+            headers: { Cookie: cookieStore.toString() },
+          });
+
+        if (isPrivateRoute)
+          return NextResponse.next({
+            headers: { Cookie: cookieStore.toString() },
+          });
       }
-
-      if (isPublicRoute) return NextResponse.next();
-
-      if (isPrivateRoute)
-        return NextResponse.redirect(new URL("/sign-in", request.url));
     }
+
+    if (isPublicRoute) return NextResponse.next();
+
+    if (isPrivateRoute)
+      return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   if (isPublicRoute) return NextResponse.redirect(new URL("/", request.url));

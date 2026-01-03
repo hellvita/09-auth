0351bc6fd8075f90f24c110c256d060a0b5bd189
hellvita <seredaolga03@gmail.com>
2026-01-03 20:53:22 +0200

    add the layout.tsx for the auth routes

---
+++ app/(auth routes)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
+  return children;
+}

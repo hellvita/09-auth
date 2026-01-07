d2670edbcd09ae34785aafd2094f3bfb6ba9b761
hellvita <seredaolga03@gmail.com>
2026-01-07 17:14:35 +0200

    remove the server-side components

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -1,5 +1,72 @@
-import SignInPageClient from "./SignInPage.client";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { login, LoginRequest } from "@/lib/api/clientApi";
+import { useAuthStore } from "@/lib/store/authStore";
+import css from "./SignInPage.module.css";
 
 export default function SignInPage() {
-  return <SignInPageClient />;
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      setError("");
+
+      const formValues = Object.fromEntries(
+        formData
+      ) as unknown as LoginRequest;
+
+      const res = await login(formValues);
+
+      if (res) {
+        setUser(res);
+        router.push("/profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError((error as Error).message ?? "Oops... some error");
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <form className={css.form} action={handleSubmit}>
+        <h1 className={css.formTitle}>Sign in</h1>
+
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        {error !== "" && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
 }
--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,14 +1,97 @@
-import { getMe } from "@/lib/api/serverApi";
-import EditProfilePageClient from "./EditProfilePage.client";
+"use client";
 
-export default async function EditProfilePage() {
-  const user = await getMe();
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getMe, updateMe } from "@/lib/api/clientApi";
+import { User } from "@/types/user";
+import Loading from "@/app/loading";
+import Image from "next/image";
+import toast from "react-hot-toast";
+import css from "./EditProfilePage.module.css";
+
+export default function EditProfilePage() {
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await getMe();
+
+        setUser(data);
+        setUserName(data.username);
+      } catch {
+        toast("Could not load profile, please try again...");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUserName(event.target.value);
+
+  const handleSave = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const res = await updateMe({
+      email: user?.email ? user.email : "",
+      username: userName,
+    });
+
+    if (res) {
+      router.push("/profile");
+    } else {
+      toast("Could not update profile, please try again...");
+    }
+  };
+
+  const handleCancel = () => router.push("/profile");
+
+  if (!user) return <Loading />;
 
   return (
-    <EditProfilePageClient
-      email={user.email}
-      username={user.username}
-      avatar={user.avatar}
-    />
+    <main className={css.mainContent}>
+      <div className={css.profileCard}>
+        <h1 className={css.formTitle}>Edit Profile</h1>
+
+        <Image
+          src={user.avatar}
+          alt="User Avatar"
+          width={120}
+          height={120}
+          className={css.avatar}
+        />
+
+        <form className={css.profileInfo} onSubmit={handleSave}>
+          <div className={css.usernameWrapper}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              className={css.input}
+              required
+              defaultValue={user.username}
+              onChange={handleChange}
+            />
+          </div>
+
+          <p>Email: {user.email}</p>
+
+          <div className={css.actions}>
+            <button
+              type="button"
+              className={css.cancelButton}
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+            <button type="submit" className={css.saveButton}>
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }

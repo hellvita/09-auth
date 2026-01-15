508695b195a08f1798fb22b7be0c971e5e533dc6
hellvita <seredaolga03@gmail.com>
2026-01-15 14:44:37 +0200

    use the Link elemnt instead og the button elemnt

--- components/EditIcon/EditIcon.tsx
+++ components/EditIcon/EditIcon.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { CiEdit } from "react-icons/ci";
 import Link from "next/link";
 import css from "./EditIcon.module.css";
@@ -11,15 +10,6 @@ interface EditIconProps {
 }
 
 export default function EditIcon({ noteId, iconSize = 16 }: EditIconProps) {
-  const router = useRouter();
-  const handleClick = () => {
-    router.push(`/notes/${noteId}/edit`);
-  };
-  return (
-    <button onClick={handleClick} className={css.btn}>
-      <CiEdit className={css.editIcon} size={iconSize} />
-    </button>
-  );
   return (
     <Link href={`/notes/${noteId}/edit`} className={css.link}>
       <CiEdit className={css.editIcon} size={iconSize} />
